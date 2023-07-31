b60d24e1ddb71f367f84e6f68ed9bacb4419efcf
Yusuke Namoto <91815422+namotoy@users.noreply.github.com>
2023-07-31 14:13:23 +0900

    edit EmployeeController

--- src/main/java/com/example/demo/app/EmployeeController.java
+++ src/main/java/com/example/demo/app/EmployeeController.java
@@ -1,13 +1,23 @@
 package com.example.demo.app;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.entity.Employee;
+import com.example.demo.service.EmployeeService;
 
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class EmployeeController {
+	
+	private final EmployeeService employeeService;
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
 	 // メニュー画面への遷移
     @GetMapping("/menu")
     public String showMenu(Model model, HttpSession session) {
@@ -19,12 +29,12 @@ public class EmployeeController {
     @GetMapping("/emp_list")
     public String showEmpList(Model model) {
         //従業員リストを取得
-//    		List<Employee> list = employeeService.findAll();
-    		
-//    		model.addAttribute("list", list);
-//    		model.addAttribute("title", "従業員一覧");
-    		return "emp_list";
-    		}
+		List<Employee> list = employeeService.findAll();
+		
+		model.addAttribute("list", list);
+		model.addAttribute("title", "従業員一覧");
+		return "emp_list";
+	}
     
     
 }
