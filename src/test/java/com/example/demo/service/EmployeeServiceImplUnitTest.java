0ee0966deca442d1ccf957e5c8c6ff0eb74728b1
Yusuke Namoto <91815422+namotoy@users.noreply.github.com>
2023-08-04 18:04:00 +0900

    edit unit test file

--- src/test/java/com/example/demo/service/EmployeeServiceImplUnitTest.java
+++ src/test/java/com/example/demo/service/EmployeeServiceImplUnitTest.java
@@ -3,6 +3,9 @@ package com.example.demo.service;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,8 +14,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.dao.EmptyResultDataAccessException;
 
+import com.example.demo.entity.Employee;
 import com.example.demo.repository.EmployeeDao;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("EmployeeServiceImplの単体テスト")
@@ -23,11 +31,47 @@ public class EmployeeServiceImplUnitTest {
 	 @InjectMocks
 	private EmployeeServiceImpl employeeServiceImpl;
 	 
+	private Validator validator;
+
+	@BeforeEach
+	public void setUp() {
+	     ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+	     validator = factory.getValidator();
+	 }
+	 
+	 @Test
+	 @DisplayName("IDが未入力の場合のテスト")
+	 public void testEmpIdNotNull() {
+	     Employee employee = new Employee();
+	     employee.setEmpId(null); // 従業員IDをnullに設定
+	     employee.setPassword("password"); // 仮のパスワードを設定
+
+	     Set<ConstraintViolation<Employee>> violations = validator.validate(employee);
+	     assertFalse(violations.isEmpty()); // バリデーションエラーが空でないことを確認
+
+	     ConstraintViolation<Employee> violation = violations.iterator().next();
+	     assertEquals("従業員IDは必須です", violation.getMessage()); // エラーメッセージが期待どおりであることを確認
+	 }
+	 
+	 @Test
+	 @DisplayName("パスワードが未入力の場合のテスト")
+	 public void testPassWordNotNull() {
+		 Employee employee = new Employee();
+		 employee.setEmpId(12345); //仮の従業員IDを設定
+		 employee.setPassword(""); //パスワードを空文字に設定
+		 
+		 Set<ConstraintViolation<Employee>> violations = validator.validate(employee);
+		 assertFalse(violations.isEmpty()); // バリデーションエラーが空でないことを確認
+		 
+		 ConstraintViolation<Employee> violation = violations.iterator().next();
+		 assertEquals("パスワードは必須です", violation.getMessage()); // エラーメッセージが期待どおりであることを確認
+	 }
+	 
 	 @Test
 	 @DisplayName("IDまたはパスワードが取得できなかった場合のテスト")
 	 void testGetIdPWThrowException() {
-		 int id = 12345;  // 例として設定
-		 String password = "password";  // 例として設定
+		 int id = 12345;  // 仮のIDを設定
+		 String password = "password";  //仮のパスワードを設定
 		 
 		 //daoクラスのfindByIdメソッドが呼び出された時、少なくとも1つの結果が戻り値として返ってくる指定
 		 when(dao.findById(id,password)).thenThrow(new EmptyResultDataAccessException(1));
