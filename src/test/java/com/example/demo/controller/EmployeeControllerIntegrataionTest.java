c31132b8bb67e9cfd111d8fe146a58a165415c8d
Yusuke Namoto <91815422+namotoy@users.noreply.github.com>
2023-08-29 09:48:19 +0900

    edit IntegrationTest file

--- src/test/java/com/example/demo/controller/EmployeeControllerIntegrataionTest.java
+++ src/test/java/com/example/demo/controller/EmployeeControllerIntegrataionTest.java
@@ -1,68 +1,147 @@
 package com.example.demo.controller;
 
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.CoreMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringJUnitConfig
 @SpringBootTest
+@AutoConfigureMockMvc
 @DisplayName("EmployeeControllerの結合テスト")
 public class EmployeeControllerIntegrataionTest {
-	@Autowired
-	//テスト対象クラス
-    private WebApplicationContext context;
 
 	//MockMVオブジェクト
-    private MockMvc mockMvc;
-    
-    @BeforeEach
-    public void setupMockMvc(){
-    	//MockMVCオブジェクトにテスト対象メソッドを設定
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
-    }
-    
-    @Test
-    @DisplayName("従業員一覧ボタンをクリックすると従業員一覧画面に遷移するテスト")
-    public void testShowEmpList() throws Exception{
-    	//"/emp_list"にGETでアクセスするリクエストを作成
-        mockMvc.perform(get("/emp_list"))  
-        	//リクエストの結果のHttpStatusがOKであることを検証
-            .andExpect(status().isOk())
-            //遷移先のviewの名前がemp_listであることを検証
-            .andExpect(view().name(is("emp_list")));
-    }
-    
-//    @Test
-//    @DisplayName("ログインしていない場合、ログイン画面に遷移するテスト")
-//    public void testNotLoginMovelogin() throws Exception{
-//    	//"/emp_list"にGETでアクセスするリクエストを作成
-//    	mockMvc.perform(get("/emp_list"))  
-//    	//リクエストの結果のHttpStatusが3xx（リダイレクト）であることを検証
-//    	.andExpect(status().is3xxRedirection())
-//    	//リダイレクト先のURLが"/login"であることを検証
-//    	.andExpect(redirectedUrl("/login"));
-//    }
-    
-    @Test
-    @DisplayName("ログインボタンをクリックするとメニュー画面に遷移するテスト")
-    public void LoginToMenuTest() throws Exception{
-    	//"/login"からPOSTでアクセスするリクエストを作成
-        mockMvc.perform(post("/login") 
-        // フォームのパラメータをセット
-	        .param("empId", "50001") 
-	        .param("password", "hana2023")) 
-        	//リクエストの結果のHttpStatusがOKであることを検証
-            .andExpect(status().is3xxRedirection())
-          //リダイレクト先のURLが"/login"であることを検証
-        	.andExpect(redirectedUrl("/menu"));    }
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	@DisplayName("ログインしていない場合に各画面へ直接アクセスできないテスト")
+	public void testRedirectToLoginWhenNotLoggedIn() throws Exception {
+		// ユーザーがログインしていない状態でemp_listへのGETリクエストを模倣
+		mockMvc.perform(MockMvcRequestBuilders.get("/StaffTrack/emp_list"))
+		// レスポンスがリダイレクトとして、指定されたURLへ遷移することを期待
+		.andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+		.andExpect(MockMvcResultMatchers.redirectedUrl("/StaffTrack/login"));
+	}
+	
+	@Test
+	@DisplayName("ログインボタンをクリックするとメニュー画面に遷移するテスト")
+	public void LoginToMenuTest() throws Exception{
+		//"/login"からPOSTでアクセスするリクエストを作成
+		mockMvc.perform(post("/StaffTrack/login") 
+				// フォームのパラメータをセット
+				.param("empId", "50002") 
+				.param("password", "1111")) 
+		//リクエストの結果のHttpStatusがOKであることを検証
+		.andExpect(status().is3xxRedirection())
+		//リダイレクト先のURLが"/menu"であることを検証
+		.andExpect(redirectedUrl("/StaffTrack/menu"));
+	}
+	
+	@Test
+	@DisplayName("従業員一覧ボタンをクリックすると従業員一覧画面に遷移するテスト")
+	public void testShowEmpList() throws Exception{
+		  //セッションを作成
+	    MockHttpSession session = new MockHttpSession();
+	    session.setAttribute("username", "validUsername");
+		//"/emp_list"にGETでアクセスするリクエストを作成
+		mockMvc.perform(get("/StaffTrack/emp_list")
+				// セッションを設定
+	            .session(session)
+				// フォームのパラメータをセット
+				.param("empId", "50002") 
+				.param("password", "1111")) 
+		//リクエストの結果のHttpStatusがOKであることを検証
+		.andExpect(status().isOk())
+		//遷移先のviewの名前がemp_listであることを検証
+		.andExpect(view().name(is("emp_list")));
+	}
+	
+	@Test
+	@DisplayName("従業員登録ボタンをクリックすると従業員登録画面に遷移するテスト")
+	public void testShowRegist() throws Exception{
+		//セッションを作成
+		MockHttpSession session = new MockHttpSession();
+		session.setAttribute("username", "validUsername");
+		//"/emp_regist"にGETでアクセスするリクエストを作成
+		mockMvc.perform(get("/StaffTrack/emp_regist")
+				// セッションを設定
+				.session(session)
+				// フォームのパラメータをセット
+				.param("empId", "50002") 
+				.param("password", "1111")) 
+		//リクエストの結果のHttpStatusがOKであることを検証
+		.andExpect(status().isOk())
+		//遷移先のviewの名前がemp_registであることを検証
+		.andExpect(view().name(is("emp_regist")));
+	}
+	
+	@Test
+	@DisplayName("従業員検索ボタンをクリックすると従業員検索画面に遷移するテスト")
+	public void testShowSearch() throws Exception{
+		//セッションを作成
+		MockHttpSession session = new MockHttpSession();
+		session.setAttribute("username", "validUsername");
+		//"/emp_search"にGETでアクセスするリクエストを作成
+		mockMvc.perform(get("/StaffTrack/emp_search")
+				// セッションを設定
+				.session(session)
+				// フォームのパラメータをセット
+				.param("empId", "50002") 
+				.param("password", "1111")) 
+		//リクエストの結果のHttpStatusがOKであることを検証
+		.andExpect(status().isOk())
+		//遷移先のviewの名前がemp_searchであることを検証
+		.andExpect(view().name(is("emp_search")));
+	}
+	
+	@Test
+	@DisplayName("従業員削除ボタンをクリックすると従業員削除画面に遷移するテスト")
+	public void testShowDelete() throws Exception{
+		//セッションを作成
+		MockHttpSession session = new MockHttpSession();
+		session.setAttribute("username", "validUsername");
+		//"/emp_delete"にGETでアクセスするリクエストを作成
+		mockMvc.perform(get("/StaffTrack/emp_delete")
+				// セッションを設定
+				.session(session)
+				// フォームのパラメータをセット
+				.param("empId", "50002") 
+				.param("password", "1111")) 
+		//リクエストの結果のHttpStatusがOKであることを検証
+		.andExpect(status().isOk())
+		//遷移先のviewの名前がemp_deleteであることを検証
+		.andExpect(view().name(is("emp_delete")));
+	}
+	
+	@Test
+	@DisplayName("従業員更新ボタンをクリックすると従業員更新画面に遷移するテスト")
+	public void testShowupdate() throws Exception{
+		//セッションを作成
+		MockHttpSession session = new MockHttpSession();
+		session.setAttribute("username", "validUsername");
+		//"/emp_update"にGETでアクセスするリクエストを作成
+		mockMvc.perform(get("/StaffTrack/emp_update")
+				// セッションを設定
+				.session(session)
+				// フォームのパラメータをセット
+				.param("empId", "50002") 
+				.param("password", "1111")) 
+		//リクエストの結果のHttpStatusがOKであることを検証
+		.andExpect(status().isOk())
+		//遷移先のviewの名前がemp_updateであることを検証
+		.andExpect(view().name(is("emp_update")));
+	}
+
 }
--- src/test/java/com/example/demo/service/EmployeeServiceImplIntegrationTest.java
+++ src/test/java/com/example/demo/service/EmployeeServiceImplIntegrationTest.java
@@ -2,11 +2,17 @@ package com.example.demo.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import com.example.demo.entity.Employee;
 
 @SpringJUnitConfig
 @SpringBootTest
@@ -16,10 +22,90 @@ public class EmployeeServiceImplIntegrationTest {
 	@Autowired
 	private EmployeeService employeeService;
 
-	
+
 	//正常系
+	@Test
+	@DisplayName("ログイン画面でIDとパスワードを入力して、ログインができるテスト")
+	public void userLoginSuccessTest() {
+		try {
+			Optional<Employee> result = employeeService.userLogin(50002, "1111");
+			assertTrue(result.isPresent());
+		} catch (EmployeeNotFoundException e) {
+			fail("テストが失敗");
+		}
+	}
+
+	//ログアウト機能はHttpSessionモックを使うため単体テストのみ
+
+	@Test
+	@DisplayName("従業員一覧リストを取得できた場合のテスト")
+	public void findAllCheckCountTest() {
+		List<Employee> list = employeeService.findAll();
+		assertEquals(4, list.size());
+	}
+
+	//	@Test
+	//	@DisplayName("従業員登録ができた場合のテスト")
+	//	public void insertSuccessTest() {
+	//		Employee employee = new Employee();
+	//        employee.setEmpId(1001); // 重複していないIDを想定
+	//		employee.setEmpName("山田太郎");
+	//		employee.setEmail("samplesample@sample.com");
+	//		employee.setBirthDate(LocalDate.of(2000, 8, 12));
+	//		employee.setSalary(300000);
+	//		employee.setDeptName("総務部");
+	//		employee.setPassword("pass2023");
+	//
+	//        try {
+	//            employeeService.insert(employee);
+	//        } catch (Exception e) {
+	//            fail("Exception should not be thrown.");
+	//        }
+	//    }
+
+	@Test
+	@DisplayName("従業員IDで検索できるテスト")
+	public void testFindByIdExistingEmployee() {
+		int existingEmpId = 50001; // データベースに存在するIDを想定
+		try {
+			Optional<Employee> employee = employeeService.findById(existingEmpId);
+			assertTrue(employee.isPresent()); // 従業員が存在することを確認
+		} catch (Exception e) {
+			fail("Exception should not be thrown.");
+		}
+	}
+
+	@Test
+	@DisplayName("従業員名で検索できる場合のテスト")
+	public void testFindByNameExistingEmployee() {
+		String existingEmpName = "山田三郎"; // データベースに存在する従業員名
+		try {
+			List<Employee> employees = employeeService.findByName(existingEmpName);
+			assertFalse(employees.isEmpty()); // 従業員が1人以上存在することを確認
+		} catch (Exception e) {
+			fail("Exception should not be thrown.");
+		}
+	}
+
+	@Test
+	@DisplayName("従業員IDで削除ができる場合のテスト")
+	public void testDeleteSuccessfully() {
+		try {
+			employeeService.delete(1001);  // 存在する従業員IDを設定
+			// 例外がスローされなければテスト成功
+		} catch (Exception e) {
+			fail("Unexpected exception type thrown.");
+		}
+	}
 	
-	
+//	@Test
+//	@DisplayName("従業員IDで変更ができる場合のテスト")
+//	public void testUpateSuccessfully() {
+//		Employee existingEmployee = new Employee();
+//        existingEmployee.setEmpId(50001); // 既存の従業員IDを仮定
+//        employeeService.update(existingEmployee); 
+//	}
+
 	//異常系
 	@Test
 	@DisplayName("IDとパスワードが取得できずログインできない場合のテスト")
@@ -31,4 +117,96 @@ public class EmployeeServiceImplIntegrationTest {
 			assertEquals("IDまたはパスワードに誤りがあります", e.getMessage());
 		}
 	}
+
+
+	@Test
+	@DisplayName("従業員一覧リストを取得できない場合のテスト")
+	public void testFindAllEmployeesWhenListIsEmpty(){
+		List<Employee> list = employeeService.findAll();
+		assertTrue(list.isEmpty(), "従業員情報の取得に失敗しました");
+	}
+
+	@Test
+	@DisplayName("従業員IDが登録できない場合のテスト")
+	public void testInsertEmployeeWithDuplicateEmpId() {
+		Employee employee = new Employee();
+		employee.setEmpId(50001); // すでにデータベースに存在するIDを想定
+		employee.setEmpName("山田太郎");
+		employee.setEmail("samplesample@sample.com");
+		employee.setBirthDate(LocalDate.of(2000, 8, 12));
+		employee.setSalary(300000);
+		employee.setDeptName("総務部");
+		employee.setPassword("pass2023");
+
+		try {
+			employeeService.insert(employee);
+			fail("Expected DuplicateEmpIdException to be thrown.");
+		} catch (DuplicateEmpIdException e) {
+			// Expected exception.
+		} catch (Exception e) {
+			fail("Unexpected exception type thrown.");
+		}
+	}
+
+	@Test
+	@DisplayName("従業員IDが存在せず検索できない場合のテスト")
+	public void testFindByIdNonExistingEmployee() {
+		int nonExistingEmpId = 9999; // データベースに存在しないIDを想定
+		try {
+			employeeService.findById(nonExistingEmpId);
+			fail("Expected EmployeeNotFoundException to be thrown.");
+		} catch (EmployeeNotFoundException e) {
+			// Expected exception.
+		} catch (Exception e) {
+			fail("Unexpected exception type thrown.");
+		}
+	}
+
+	@Test
+	@DisplayName("従業員名が存在せず検索できない場合のテスト")
+	public void testFindByNameNonExistingEmployee() {
+		String nonExistingEmpName = "hoge"; // データベースに存在しない従業員名
+		try {
+			List<Employee> result = employeeService.findByName(nonExistingEmpName);
+			if (result == null || result.isEmpty()) {
+				throw new EmployeeNotFoundException("検索条件に該当する従業員は見つかりません");
+			}
+			fail("Expected EmployeeNotFoundException to be thrown.");
+		} catch (EmployeeNotFoundException e) {
+			// Expected exception.
+		} catch (Exception e) {
+			fail("Unexpected exception type thrown.");
+		}
+	}
+
+	@Test
+	@DisplayName("従業員IDが存在せず削除できない場合のテスト")
+	public void testDeleteWhenEmployeeDoesNotExist() {
+		try {
+			employeeService.delete(1);  // 1は存在しない従業員IDと仮定
+			fail("Expected EmployeeNotFoundException to be thrown.");
+		} catch (EmployeeNotFoundException e) {
+			assertEquals("削除する従業員が見つかりません", e.getMessage());
+		} catch (Exception e) {
+			fail("Unexpected exception type thrown.");
+		}
+	}
+	
+//	@Test
+//	@DisplayName("従業員IDが存在せず変更できない場合のテスト")
+//	public void testUpdateFailure() {
+//		Employee nonExistingEmployee = new Employee();
+//		nonExistingEmployee.setEmpId(10); // 存在しない従業員IDを仮定
+//
+//		boolean hasException = false;
+//		try {
+//			employeeService.update(nonExistingEmployee);
+//		} catch (EmployeeNotFoundException e) {
+//			hasException = true;
+//		} catch (Exception e) {
+//			fail("Unexpected exception type thrown.");
+//		}
+//		assertTrue(hasException, "Expected EmployeeNotFoundException to be thrown.");
+//	}
+
 }
